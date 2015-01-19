9091d857a55f27058352fb1d9e5a435abbef40d6
黎静北 <jingbei.li@yahoo.com>
2015-01-19 17:56:13 +0800

    删除：     match.sh

--- prebuild.sh
+++ prebuild.sh
@@ -1,22 +1,26 @@
-# Matching
-[ ! -d match ] && mkdir match
-[ -f missing.txt ] && rm missing.txt
-for i in origin/*.lab
+# Matching .lab and .f0_ascii
+[ ! -d match/lab ] && mkdir -p match/lab
+[ ! -d match/f0_ascii ] && mkdir -p match/f0_ascii
+rm -f missing-list.txt
+cd orgin
+for i in *.lab
 do
 	j=${i%.lab}.f0_ascii
 	if [ -f $j ]
 	then
-		cp $i match/
-		cp $j match/
+		cp $i ../match/lab
+		cp $j ../match/f0_ascii
 	else
-		echo $j >> missing.txt
+		echo $j >> ../missing-list.txt
 	fi
 done
+cd ..
 
 # Genernating f0files
 [ ! -d match/f0files ] && mkdir -p match/f0files
-cd match
+cd match/f0_ascii
 for i in *.f0_ascii
 do
-	cat $i |awk '{print $1}' > f0files/$i
+	cat $i |awk '{print $1}' > ../f0files/$i
 done
+cd ../..
