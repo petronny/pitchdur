dfdb8150f22a15f37abda2957999bb36aab173f8
Jingbei Li <i@jingbei.li>
2015-03-19 17:25:38 +0800

    text_loader能正确统计中文字数了

--- text-loader.m
+++
@@ -1,6 +0,0 @@
-input=fopen('origin/2000Script(minimal)_checked_sp_sy.txt','r');
-while ~feof(input)
-	text=fgets(input);
-	fprintf('%s',text);
-end
-close(input);
--- text_loader.m
+++ text_loader.m
@@ -2,5 +2,22 @@ input=fopen('origin/2000Script(minimal)_checked_sp_sy.txt','r');
 while ~feof(input)
 	text=fgets(input);
 	fprintf('%s',text);
+	start=1;
+	while text(start)~=32
+		start=start+1;
+	end
+	start=start+1;
+	count=0;
+	for i=start:length(text)
+		hex=dec2hex(text(i));
+		if length(hex)==4
+			count=count+1;
+		end
+		if length(hex)==2 && strcmp(hex,'2F')
+			fprintf('%d ',count);
+			count=0;
+		end
+	end
+	fprintf('\n');
 end
 close(input);
