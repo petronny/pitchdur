d319ab3cd5d0c9903a52dcf66c6ce9c73a5cb867
Jingbei Li <i@jingbei.li>
2015-03-20 16:23:45 +0800

    text_loader fully implemented.

--- text_loader.m
+++ text_loader.m
@@ -1,23 +1,54 @@
 input=fopen('origin/2000Script(minimal)_checked_sp_sy.txt','r');
+output=fopen('origin/2000_sp_sy.txt','w');
+pline=0;
 while ~feof(input)
 	text=fgets(input);
-	fprintf('%s',text);
 	start=1;
-	while text(start)~=32
+	while start<=length(text) && text(start)~='.' 
 		start=start+1;
 	end
-	start=start+1;
+	start=start+2;
 	count=0;
-	for i=start:length(text)
-		hex=dec2hex(text(i));
-		if length(hex)==4
-			count=count+1;
+	label='';
+	stop='';
+	if start<=length(text)
+		line=str2num(text(1:start-3));
+		if line~=pline+1
+			fprintf('%d ',line-1);
 		end
-		if length(hex)==2 && strcmp(hex,'2F')
-			fprintf('%d ',count);
-			count=0;
+		for i=start:length(text)
+			hex=dec2hex(text(i));
+			if length(hex)==4 || i==length(text)
+				if length(label)~=0
+					for j=1:count
+						if j==count
+							if length(stop)==0
+								stop=' ';
+							end
+							fprintf(output,'%s%s',label,stop);
+						else
+							fprintf(output,'%s-',label);
+						end
+					end
+					count=0;
+					label='';
+					stop='';
+				end
+				count=count+1;
+			end
+			if length(hex)==2
+				if text(i)=='|'
+					stop='|';
+				end
+				if text(i)<='z' && text(i)>='a'
+					label=[label text(i)];
+				end
+			end
 		end
+		fprintf(output,'\n');
+		pline=line;
 	end
-	fprintf('\n');
 end
-close(input);
+fprintf('\n');
+fclose(input);
+fclose(output);
